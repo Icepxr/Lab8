5372b39b69ee2c9961e764be11abb4f493b3da77
Daiyaz <uchihachikk3@gmail.com>
2024-12-06 15:37:49 +0700

    Done_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,47 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int age,height;
+    long long bounty;
+    string cha;
+    cout << "Enter your age: ";
+    cin >> age;
+    
+   
+    
+
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height<100){
+            cha = "Chopper";
+        }else if (height<180){
+            cha = "Usopp";
+        }else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty>=1111000000){
+                cha = "Zoro";
+            }else{
+                cha = "Sanji";
+            }
+        }
+    }else if(age <=60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty>500000000){
+            cha = "Jinbe";
+        }else{
+            cha = "Franky";
+        }
+    }else{
+        cha = "Brook";
+    }
+
+    cout << "Your character = "<< cha;
+
+ }
